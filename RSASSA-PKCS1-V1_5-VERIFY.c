14a869b4e43153b96288a60bd44340672f60b45e
Kristoffer Jensen <krijen16@student.aau.dk>
2021-03-23 11:08:11 +0100

    encrypt -> decrypt + PROPERTY 5 checks

--- RSASSA-PKCS1-V1_5-VERIFY.c
+++ RSASSA-PKCS1-V1_5-VERIFY.c
@@ -2,20 +2,12 @@
 #include "RSASSA-PKCS1-V1_5-VERIFY.h"
 #include "memory.h"
 
-char* __hash_message; 
-
-int32_t* RSA_3072_ENCRYPT(char* data, int size, int32_t exponent, int32_t* modulus){
-  
-  __CPROVER_assert(data == __hash_message,
-  "PROPERTY 5: The data to be encrypted is == to the computed hash");    
-  
-  int32_t* encrypt = malloc(size);
-  return encrypt;
+char* RSA_3072_DECRYPT(int32_t* signature, int signature_len, int32_t exponent, int32_t* modulus){
+  char* decrypt = malloc(256/8); //model it to be ok for PROPERTY 5
+  return decrypt;
 }
 
-
 int RSASSA_PKCS1_V1_5_VERIFY(int32_t exponent, int32_t* modulus, char* message, int message_len, int32_t* signature, int signature_len, rom_ext_manifest_t __current_rom_ext_mf){
-  
   __CPROVER_assert(__CPROVER_OBJECT_SIZE(message) == message_len,
   "PROPERTY 5: Formal parameter message_len lenght matches actual message length.");
 
@@ -29,34 +21,36 @@ int RSASSA_PKCS1_V1_5_VERIFY(int32_t exponent, int32_t* modulus, char* message, 
   "PROPERTY 5: Public key exponent is 32 bits.");
 
   __CPROVER_assert((sizeof(pub_key_t) - sizeof(exponent)) * 8 == RSA_SIZE*32,
-  "PROPERTY 5: Public key modulus is 3072-bits.");    
+  "PROPERTY 5: Public key modulus is 3072-bits."); 
 
   __REACHABILITY_CHECK
 
   if(signature_len != RSA_SIZE){
     __CPROVER_assert(signature_len * 32 != __CPROVER_OBJECT_SIZE(signature) * 8,
-    "PROPERTY 5: Length checking: If the length of the signature is not 3072 bytes, stop.");    
+    "PROPERTY 5: Length checking: If the length of the signature is not 3072 bytes, stop."); 
     __REACHABILITY_CHECK // Not reachable atm
 
     return 0;
   }
   __REACHABILITY_CHECK
 
+  int32_t* decrypt = RSA_3072_DECRYPT(signature, signature_len, exponent, modulus);
   char* hash = SHA2_256(message, message_len, __current_rom_ext_mf); //message_len in bytes
 
-  __hash_message = hash; //for PROPERTY 5
+  __CPROVER_assert(__CPROVER_OBJECT_SIZE(decrypt)==256/8, 
+  "PROPERTY 5: Decrypted message is 256 bits"); 
+
+  __CPROVER_assert(__CPROVER_r_ok(decrypt, 256/8),
+  "PROPERTY 5: Decrypted message is in readable address"); 
 
   __CPROVER_assert(__CPROVER_OBJECT_SIZE(hash)==256/8, 
-  "PROPERTY 3: Hash is 256 bits");   
+  "PROPERTY 3: Hash is 256 bits"); 
 
   __CPROVER_assert(__CPROVER_r_ok(hash, 256/8),
-  "PROPERTY 3: hash is in readable address");  
+  "PROPERTY 3: hash is in readable address"); 
 
-  int32_t* encrypt = RSA_3072_ENCRYPT(hash, RSA_SIZE*sizeof(int32_t), exponent, modulus);
-
-  if(memcmp(signature, encrypt, RSA_SIZE*sizeof(int32_t))==0)
+  if(memcmp(hash, decrypt, RSA_SIZE*sizeof(int32_t))==0)
     return 1; //verified
   else
     return 0;
 }
-
