866f30498c61eda00865dfb3a825c8a27e9d8ac9
Kristoffer Jensen <krijen16@student.aau.dk>
2021-04-14 10:23:40 +0200

    Assert to 3072 bits

--- RSASSA-PKCS1-V1_5-VERIFY.c
+++ RSASSA-PKCS1-V1_5-VERIFY.c
@@ -11,7 +11,7 @@ int RSASSA_PKCS1_V1_5_VERIFY(int32_t exponent, int32_t* modulus, char* message, 
 	__CPROVER_assert(__CPROVER_OBJECT_SIZE(message) == message_len,
 	"PROPERTY 5: Formal parameter message_len lenght matches actual message length.");
 
-	__CPROVER_assert(__CPROVER_OBJECT_SIZE(signature) * 8 == RSA_SIZE * 32,
+	__CPROVER_assert(__CPROVER_OBJECT_SIZE(signature) * 8 == 3072,
 	"PROPERTY 5: Signature to be verified is 3072-bits.");
 
 	__CPROVER_assert(__CPROVER_OBJECT_SIZE(signature) == signature_len * sizeof(int32_t),
@@ -20,13 +20,13 @@ int RSASSA_PKCS1_V1_5_VERIFY(int32_t exponent, int32_t* modulus, char* message, 
 	__CPROVER_assert(sizeof(exponent) * 8 == 32,
 	"PROPERTY 5: Public key exponent is 32 bits.");
 
-	__CPROVER_assert((sizeof(pub_key_t) - sizeof(exponent)) * 8 == RSA_SIZE*32,
+	__CPROVER_assert((sizeof(pub_key_t) - sizeof(exponent)) * 8 == 3072,
 	"PROPERTY 5: Public key modulus is 3072-bits."); 
 
 	__REACHABILITY_CHECK
 
 	if(signature_len != RSA_SIZE){
-		__CPROVER_assert(signature_len * 32 != __CPROVER_OBJECT_SIZE(signature) * 8,
+		__CPROVER_assert(signature_len * 32 != 3072,
 		"PROPERTY 5: Length checking: If the length of the signature is not 3072 bytes, stop."); 
 		__REACHABILITY_CHECK // Not reachable atm
 
