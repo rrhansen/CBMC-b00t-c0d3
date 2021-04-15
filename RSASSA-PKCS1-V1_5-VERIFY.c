046d57e7862cb14f00d6146cf670a7c52d2d6200
User1827319851 <24315940+User12837985123@users.noreply.github.com>
2021-04-15 14:21:05 +0200

    changed 3072 to RSASIZE*32

--- RSASSA-PKCS1-V1_5-VERIFY.c
+++ RSASSA-PKCS1-V1_5-VERIFY.c
@@ -54,7 +54,7 @@ int RSASSA_PKCS1_V1_5_VERIFY(int32_t exponent, int32_t* modulus, char* message, 
 	__CPROVER_assert(__CPROVER_OBJECT_SIZE(message) == message_len,
 	"PROPERTY 5: Formal parameter message_len lenght matches actual message length.");
 
-	__CPROVER_assert(__CPROVER_OBJECT_SIZE(signature) * 8 == 3072,
+	__CPROVER_assert(__CPROVER_OBJECT_SIZE(signature) * 8 == RSA_SIZE*32,
 	"PROPERTY 5: Signature to be verified is 3072-bits.");
 
 	__CPROVER_assert(__CPROVER_OBJECT_SIZE(signature) == signature_len * sizeof(int32_t),
@@ -63,7 +63,7 @@ int RSASSA_PKCS1_V1_5_VERIFY(int32_t exponent, int32_t* modulus, char* message, 
 	__CPROVER_assert(sizeof(exponent) * 8 == 32,
 	"PROPERTY 5: Public key exponent is 32 bits.");
 
-	__CPROVER_assert((sizeof(pub_key_t) - sizeof(exponent)) * 8 == 3072,
+	__CPROVER_assert((sizeof(pub_key_t) - sizeof(exponent)) * 8 == RSA_SIZE*32,
 	"PROPERTY 5: Public key modulus is 3072-bits."); 
 
 	__CPROVER_assert(__check_params(exponent, modulus, message, message_len, signature, signature_len, __current_rom_ext_mf),
@@ -72,7 +72,7 @@ int RSASSA_PKCS1_V1_5_VERIFY(int32_t exponent, int32_t* modulus, char* message, 
 	__REACHABILITY_CHECK
 
 	if(signature_len != RSA_SIZE){
-		__CPROVER_assert(signature_len * 32 != 3072,
+		__CPROVER_assert(signature_len * 32 != RSA_SIZE*32,
 		"PROPERTY 5: Length checking: If the length of the signature is not 3072 bytes, stop."); 
 		__REACHABILITY_CHECK // Not reachable atm
 
