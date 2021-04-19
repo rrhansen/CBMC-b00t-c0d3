8a2cf4b076f7de2c43c302658794ef063a5cece5
User1827319851 <24315940+User12837985123@users.noreply.github.com>
2021-04-19 09:53:10 +0200

    merge proof harnesses

--- mask_rom.c
+++ mask_rom.c
@@ -9,7 +9,6 @@ doc/security/specs/secure_boot/index.md
 */
 #include "sha2-256.h"
 #include "mask_rom.h"
-#include "verify.h"
 
 
 //Whitelist in ROM
@@ -41,6 +40,152 @@ typedef void(fail_func)(void);
 
 // Function type for entry point of boot policy fail rom ext terminated function.
 typedef void(fail_rom_ext_terminated_func)(rom_ext_manifest_t); 
+
+
+//////////////////////////////////////////////
+
+int verify_rom_ext_signature(pub_key_t rom_ext_pub_key, rom_ext_manifest_t manifest) {
+	__CPROVER_precondition(MAX_IMAGE_LENGTH > manifest.image_length && manifest.image_length > 0,
+		"Precondition: Assumes rom ext image code is < 10 and > 0");
+
+	__CPROVER_precondition(__CPROVER_r_ok(manifest.image_code, manifest.image_length),
+		"Precondition: Assumes rom ext image code is readable");
+
+	__verify_signature_called[__current_rom_ext] = 1;
+
+	int bytes =
+		sizeof(manifest.pub_signature_key) + sizeof(manifest.image_length) + manifest.image_length;
+
+	char message[bytes];
+
+	memcpy(
+		message,
+		&manifest.pub_signature_key,
+		sizeof(manifest.pub_signature_key)
+	);
+	memcpy(
+		message + sizeof(manifest.pub_signature_key),
+		&manifest.image_length,
+		sizeof(manifest.image_length)
+	);
+	memcpy(
+		message + sizeof(manifest.pub_signature_key) + sizeof(manifest.image_length),
+		manifest.image_code,
+		manifest.image_length
+	);
+
+	//Otherwise OBJECT_SIZE returns size of manifest and not signature.
+	signature_t signature = manifest.signature;
+
+	int result = RSASSA_PKCS1_V1_5_VERIFY(rom_ext_pub_key.exponent, rom_ext_pub_key.modulus, message, bytes, signature.value, RSA_SIZE, manifest);
+
+	__valid_signature[__current_rom_ext] = result;
+
+	return result; //0 or 1
+}
+
+
+int __is_valid_params(int32_t exponent, int32_t* modulus, char* message, int message_len, int32_t* signature, int signature_len, rom_ext_manifest_t __current_rom_ext_mf) {
+
+	if (exponent != __current_rom_ext_mf.pub_signature_key.exponent)
+		return 0;
+
+	if (memcmp(modulus,
+		__current_rom_ext_mf.pub_signature_key.modulus,
+		RSA_SIZE) != 0)
+		return 0;
+
+	if (memcmp(signature,
+		__current_rom_ext_mf.signature.value,
+		RSA_SIZE) != 0)
+		return 0;
+
+	if (memcmp(message,
+		&__current_rom_ext_mf.pub_signature_key,
+		sizeof(__current_rom_ext_mf.pub_signature_key)) != 0)
+		return 0;
+
+	if (memcmp(
+		message + sizeof(__current_rom_ext_mf.pub_signature_key),
+		&__current_rom_ext_mf.image_length,
+		sizeof(__current_rom_ext_mf.image_length)) != 0)
+		return 0;
+
+	if (memcmp(
+		message + sizeof(__current_rom_ext_mf.pub_signature_key) + sizeof(__current_rom_ext_mf.image_length),
+		__current_rom_ext_mf.image_code,
+		__current_rom_ext_mf.image_length) != 0)
+		return 0;
+
+
+	return 1;
+}
+
+
+char* RSA_3072_DECRYPT(int32_t* signature, int signature_len, int32_t exponent, int32_t* modulus) {
+	char* decrypt = malloc(256 / 8); //model it to be ok for PROPERTY 5
+	return decrypt;
+}
+
+int RSASSA_PKCS1_V1_5_VERIFY(int32_t exponent, int32_t* modulus, char* message, int message_len, int32_t* signature, int signature_len, rom_ext_manifest_t __current_rom_ext_mf) {
+	__CPROVER_assert(__CPROVER_OBJECT_SIZE(message) == message_len,
+		"PROPERTY 5: Formal parameter message_len lenght matches actual message length.");
+
+	__CPROVER_assert(__CPROVER_OBJECT_SIZE(signature) * 8 == 3072,
+		"PROPERTY 5: Signature to be verified is 3072-bits.");
+
+	__CPROVER_assert(__CPROVER_OBJECT_SIZE(signature) == signature_len * sizeof(int32_t),
+		"PROPERTY 5: Formal parameter signature lenght matches actual signature length.");
+
+	__CPROVER_assert(sizeof(exponent) * 8 == 32,
+		"PROPERTY 5: Public key exponent is 32 bits.");
+
+	__CPROVER_assert((sizeof(pub_key_t) - sizeof(exponent)) * 8 == 3072,
+		"PROPERTY 5: Public key modulus is 3072-bits.");
+
+	__CPROVER_assert(__is_valid_params(exponent, modulus, message, message_len, signature,
+		signature_len, __current_rom_ext_mf),
+		"PROPERTY 5: Check that key, signature, and message matches those from the manifest.");
+
+	__REACHABILITY_CHECK
+
+		if (signature_len != RSA_SIZE) {
+			__CPROVER_assert(signature_len * 32 != 3072,
+				"PROPERTY 5: Length checking: If the length of the signature is not 3072 bytes, stop.");
+			__REACHABILITY_CHECK // Not reachable atm
+
+				return 0;
+		}
+	__REACHABILITY_CHECK
+
+	char* decrypt = RSA_3072_DECRYPT(signature, signature_len, exponent, modulus);
+	char* hash = SHA2_256(message, message_len, __current_rom_ext_mf); //message_len in bytes
+
+	__CPROVER_assert(!__CPROVER_array_equal(decrypt, signature),
+		"PROPERTY 5: Decrypted signature is different from signature");
+
+	__CPROVER_assert(!__CPROVER_array_equal(hash, message),
+		"PROPERTY 5: Hash is different from original message");
+
+	__CPROVER_assert(__CPROVER_OBJECT_SIZE(decrypt) == 256 / 8,
+		"PROPERTY 5: Decrypted message is 256 bits");
+
+	__CPROVER_assert(__CPROVER_r_ok(decrypt, 256 / 8),
+		"PROPERTY 5: Decrypted message is in readable address");
+
+	__CPROVER_assert(__CPROVER_OBJECT_SIZE(hash) == 256 / 8,
+		"PROPERTY 3: Hash is 256 bits");
+
+	__CPROVER_assert(__CPROVER_r_ok(hash, 256 / 8),
+		"PROPERTY 3: hash is in readable address");
+
+	if (memcmp(hash, decrypt, RSA_SIZE * sizeof(int32_t)) == 0)
+		return 1; //verified
+	else
+		return 0;
+}
+
+//////////////////////////////////////////////
 
 
 boot_policy_t read_boot_policy() {}
@@ -129,8 +274,8 @@ void __some_entry_func() { __rom_ext_called[__current_rom_ext] = 1; /*for CBMC P
 
 int final_jump_to_rom_ext(rom_ext_manifest_t current_rom_ext_manifest) { // Returns a boolean value.
 	//This assumption causes reachability checks to succeed. If == is =, then they fail.
-	__CPROVER_assume(current_rom_ext_manifest.image_code == &__some_entry_func); //for cbmc
-	
+	//__CPROVER_assume(current_rom_ext_manifest.image_code == &__some_entry_func); //for cbmc
+	current_rom_ext_manifest.image_code = &__some_entry_func;
 	//Execute rom ext code step 2.iii.e
 	rom_ext_boot_func* rom_ext_entry = (rom_ext_boot_func*)current_rom_ext_manifest.image_code;
 
@@ -160,15 +305,6 @@ void boot_failed_rom_ext_terminated(boot_policy_t boot_policy, rom_ext_manifest_
 	__REACHABILITY_CHECK
 	fail_rom_ext_terminated_func* fail_func_entry = (fail_rom_ext_terminated_func*)boot_policy.fail_rom_ext_terminated;
 	fail_func_entry(current_rom_ext_manifest);
-}
-
-int verify_rom_ext_signature(pub_key_t rom_ext_pub_key, rom_ext_manifest_t manifest)
-{
-	__REACHABILITY_CHECK
-	int __result;
-	__valid_signature[__current_rom_ext] = __result;
-	__verify_signature_called[__current_rom_ext] = 1;
-	return __result;
 }
 
 
@@ -286,6 +422,7 @@ void PROOF_HARNESS() {
 	
 	for(int i = 0; i < rom_exts_to_try.size; i++){
 		__CPROVER_assume(MAX_IMAGE_LENGTH > rom_exts_to_try.rom_exts_mfs[i].image_length && rom_exts_to_try.rom_exts_mfs[i].image_length > 0);
+		rom_exts_to_try.rom_exts_mfs[i].image_code = malloc(sizeof(char) * rom_exts_to_try.rom_exts_mfs[i].image_length);
 	}
 
 	mask_rom_boot(boot_policy, rom_exts_to_try);
